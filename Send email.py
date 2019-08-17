91b4f226a6b840ecfa6ec9a8c8d0ec2689292ff7
Chao Ding <dingchaoruc8868@gmail.com>
2019-08-17 18:11:32 -0400

    use promt password

--- Send email.py
+++ Send email.py
@@ -1,1 +1,20 @@
 import os
+import smtplib
+import getpass
+
+EMAIL_ADDRESS = os.environ.get('test.chao8868@gmail.com') 
+EMAIL_PASSWORD = getpass.getpass()
+
+with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    
+    subject = 'Grab dinner this weekend'
+    body = 'How about dinner at 6pm this Saturday?'
+    
+    msg = f'Subject:{subject}\n\n{body}'
+    
+    smtp.sendmail(EMAIL_ADDRESS, 'dingchaoruc8868@gmail.com', msg)
